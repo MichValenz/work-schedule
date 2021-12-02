dc4c52254ac8f24d8a73995da0a71b1cb6892235
MichValenz <michvalenz27@gmail.com>
2021-12-01 20:37:04 -0600

    added date via moment in javascript file and added time blocks in html

---
+++ Assets/script.js
@@ -0,0 +1,18 @@
+
+//add current date to the top of the page
+var today = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(today);
+
+//global variables 
+
+
+
+//time blocks with standard business hours
+
+
+//color code past, presenet, and future each time block
+
+
+//add ability to add event when time block is clicked
+
+//add save button function which is saved to local storage and after refresh events should be stored
