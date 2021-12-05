9a3e78f3658b5a5b6c76fd2cf50b06638b639c0e
MichValenz <michvalenz27@gmail.com>
2021-12-04 23:52:54 -0600

    added local storage function. app completed

--- Assets/script.js
+++ Assets/script.js
@@ -1,18 +1,74 @@
+// 1. add current date to the top of the page
+const clock = document.getElementById('currentDay');
+    setInterval(() => {
+        const now = moment();
+        const clockDisplay = now.format('dddd, MMM Do YYYY, h:mm:ss a')
+        clock.textContent = clockDisplay;
+    }, 1000);
 
-//add current date to the top of the page
-var today = moment().format('dddd, MMM Do YYYY');
-$("#currentDay").html(today);
 
-//global variables 
+    
+// global variable
+var saveBtn = $(".saveBtn");
+
+saveBtn.on("click", function() {
+
+    var time = $(this).siblings(".hour").text();
+    var taskDescription = $(this).siblings(".description").val();
+
+    // THEN the text for that event is saved in local storage
+    localStorage.setItem(time, taskDescription);
+
+});
 
 
-
+// 2. color code past, present, and future each time block
 //time blocks with standard business hours
+function colorTime() {
+        var currentTime = moment().hour();
+        $(".time-block").each(function () {
+            var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+            if (timeBlock < currentTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (timeBlock === currentTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
 
 
-//color code past, presenet, and future each time block
+// 4.  after refresh events should stay persistent
+
+function saveInput() {
+
+    $(".hour").each(function() {
+        var theTask = $(this).text();
+        var toDo = localStorage.getItem(theTask);
+
+        if(toDo) {
+        $(this).siblings(".description").val(toDo);
+        }
+    });
+}
 
 
-//add ability to add event when time block is clicked
+colorTime();
+saveInput();
 
-//add save button function which is saved to local storage and after refresh events should be stored
+
+
+
+
+
+
